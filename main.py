8ec971b0cc3d4bfd97421b15ab943f4ccdaf87b3
Valceanu <marioxnoa76@gmail.com>
2023-11-25 12:37:49 +0200

    Am fixat bug-ul cu alegerea domeniului de 2 ori. Si am adaugat sunete!

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import os  # Pentru A Sterge Consola
 import random
+import winsound # Pentru Audio
 from Cuvinte import set_cuvinte
 
 # \n spune programului sa lase spatiu (endl in c++ sau <br> in html)
-
 
 # Arata o litera din cuvant
 def indiciu():
@@ -21,14 +21,12 @@ def alegeCuvantRandom():
   lista = random.choice(list(set_cuvinte.keys()))
   cuvant = random.choice(set_cuvinte[lista])
 
-# Meniul Principal
+# Meniul Principal, Turorial
 def mainGameIntroduction():
   from ASCII__art import logo
-  alegeCuvantRandom()
   print(logo)
   print("\n")
   print("Bine ati venit la jocul de spanzuratoare!")
-  print(f"Domeniul : {lista} \n")
   print("Daca te blochezi la un cuvant, scrie indiciu in terminal!")
   print("Ai doua indicii pe runda! \n")
   input("Apasa enter pentru a incepe jocul!").lower()
@@ -39,10 +37,15 @@ def mainGameIntroduction():
 # Jocul Principal
 def mainGame():
   os.system('cls')
+  
+  # Numarul 1 este pentru a se auzi in background, de mentionat: functia nu suporta decat format .wav
+  winsound.PlaySound('Audio/Game Win Sound.wav', 1)
 
   # Pregatirea jocului
 
   alegeCuvantRandom()
+  print(f"Domeniul : {lista} \n")
+
   global numar_indicii
   numar_indicii = 2
   
@@ -81,47 +84,54 @@ def mainGame():
 
     # Da un indiciu 
     # elif este similar cu else if in C++
-
     if ((litera_introdusa == "indiciu") and (numar_indicii > 0)):
       print(f"{''.join(linii)} \n")
 
       numar_indicii = numar_indicii - 1
       if (numar_indicii == 0):
-        print("Ai epuizat numarul de indicii! \n")
+        print("Nu mai ai indicii ramase! \n")
       else:
+        winsound.PlaySound('Audio/Game Hint Sound.wav', 1)
         print(f"Mai ai {numar_indicii} indiciu ramas! \n")
       
       indiciu()
     elif ((litera_introdusa == "indiciu") and (numar_indicii <= 0)):
-      print(f"{''.join(linii)} \n")
-      print(f"Ai epuizat numarul de indicii! \n")
+        print(f"{''.join(linii)} \n")
+        print("Ai epuizat deja numarul de indicii! \n")
     else:
-      # Litera introdusa nu se gaseste in cuvant
+      # Caz 1: Litera introdusa nu se gaseste in cuvant
 
       if litera_introdusa not in cuvant:
         vieti -= 1
         print(f"Litera {litera_introdusa} nu face parte din cuvant.")
 
-        # Nu mai sunt vieti, runda s-a terminat
+        if vieti > 0:
+          winsound.PlaySound('Audio/Game Error Sound.wav', 1)
 
+        # Nu mai sunt vieti, runda s-a terminat
         if vieti == 0:
+          winsound.PlaySound('Audio/Game Lose Sound.wav', 1)
           os.system('cls')
           sfarsit_joc = True
           print(stages[0])
           print("Ai pierdut.")
           print(f"Cuvantul era {cuvant}. \n")
 
-      # Nu au mai ramas spatii necompletate. Runda s-a terminat
+      # Caz 2:  Nu au mai ramas spatii necompletate. Runda s-a terminat
 
       if "_" not in linii:
+        winsound.PlaySound('Audio/Game Win Sound.wav', 1)
         os.system('cls')
         sfarsit_joc = True
         print("Ai ghicit cuvantul! \n")
 
-      # Litera deja in cuvant
+      # Caz 3:  Litera deja in cuvant
 
       if litera_introdusa in cuvant:
         print(f"Litera {litera_introdusa} este corecta (sau este deja ghicita).")
+
+        if not sfarsit_joc:
+          winsound.PlaySound('Audio/Generic Game Notification Sound.wav', 1)
 
       # Unesc toate elementele listei "linii" pentru a le transforma intr-un string
 
