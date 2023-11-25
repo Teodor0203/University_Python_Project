9ce532ca06ebbbe096fd1bc54cbadf943ba14362
Teodor0203 <148296115+Teodor0203@users.noreply.github.com>
2023-11-25 13:54:31 +0200

    Bug lista

--- Cuvinte.py
+++ Cuvinte.py
@@ -1,6 +1,6 @@
 set_cuvinte = { 
 
-'animale' :['arici','aligator','acvila','barza','balena','bibilica','bizon','bursuc','camila','crocodil','cal','ciocanitoare','delfin''dihor''elefant','echidna','fazan''ghepard',
+'animale' :['arici','aligator','acvila','barza','balena','bibilica','bizon','bursuc','camila','crocodil','cal','ciocanitoare','delfin','dihor','elefant','echidna','fazan','ghepard',
             'iepure','jder','jaguar','lebada','leopard','maimuta','nurca','castor','veverita','zebra','impala','tigru','leu','antilopa','caprioara','pisica','pantera','cangur','vulpe'],
 'masini' : ['volvo', 'mercedes', 'renault', 'subaru', 'volkswagen', 'hyundai', 'maserati', 'ferrari', 'porsche', 'tesla', 'koenigsegg', 'lamborghini', 'bugatti', 'dodge', 'mustang'],
              
