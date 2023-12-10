0b91907c8b74225dc07c2b43451ed47985758add
Teodor0203 <148296115+Teodor0203@users.noreply.github.com>
2023-12-10 13:38:13 +0200

    Fereastra

--- fereastra.py
+++ fereastra.py
@@ -1,38 +1,45 @@
 import pygame, sys
 from pygame import mixer
 from buton import Button
+from main import HangmanGame
 
 mixer.init()
 mixer.music.load("Audio/birds-19624.mp3")
 mixer.music.set_volume(0.7)
 mixer.music.play()
 
-
 pygame.init()
+
 
 ecran = pygame.display.set_mode((1920, 1080))
 pygame.display.set_caption("Spanzuratoarea")
 
-BG = pygame.image.load("interfata/bg.png")
-
+BG = pygame.image.load("interfata/bg2.jpg")
+screenUpdate = pygame.transform.scale(BG, (1920, 1080))
 def font(size):
-    return pygame.font.Font("interfata/font.ttf", size)
+    return pygame.font.Font("interfata/font2.otf", size)
 
 def joaca():
+    joc = HangmanGame()
+    joc.alege_cuvant_random()
+    joc.indiciu()
     while True:
         pozitie_mouse_joca = pygame.mouse.get_pos()
 
-        ecran.blit(BG, (0,0))
+        ecran.blit(screenUpdate, (0, 0))
 
-        joaca_text = font(45).render("Aici va fi jocul", True, "blueviolet")
+        joaca_text = font(45).render("Bine ati venit la jocul de spanzuratoare!!", True, "black")
         joaca_rect = joaca_text.get_rect(center=(960, 300))
         ecran.blit(joaca_text, joaca_rect)
 
-        joaca_inapoi = Button(image=None, pos=(960, 500), text_input="INAPOI", font=font(75), base_color="blueviolet", hovering_color="blue")
-        joaca_indiciu = Button(image=None, pos=(1900,100), text_input="?", font=font(75),base_color="black", hovering_color="white")
+        joaca_inainte = Button(image=None, pos=(960, 500), text_input="INAINTE", font=font(55), base_color="black", hovering_color="white")
+        joaca_inapoi = Button(image=None, pos=(960, 650), text_input="INAPOI", font=font(55), base_color="black", hovering_color="white")
 
-        joaca_indiciu.changeColor(pozitie_mouse_joca)
-        joaca_indiciu.update(ecran)
+
+        joaca_inainte.changeColor(pozitie_mouse_joca)
+        joaca_inainte.update(ecran)
+
+
 
         joaca_inapoi.changeColor(pozitie_mouse_joca)
         joaca_inapoi.update(ecran)
@@ -45,22 +52,63 @@ def joaca():
                 if joaca_inapoi.checkForInput(pozitie_mouse_joca):
                     meniu_principal()
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if joaca_indiciu.checkForInput(pozitie_mouse_joca):
-                    indiciu()
+                if joaca_inainte.checkForInput(pozitie_mouse_joca):
+                    continua_1(joc)
         pygame.display.update()
 
-def indiciu():
+def continua_1(joc):
+
+    while True:
+        pozitie_mouse_continua = pygame.mouse.get_pos()
+
+        ecran.blit(screenUpdate, (0, 0))
+
+        for litera in range(len(joc.cuvant)):
+            text_litera = font(85).render("_", True, "black")
+            ecran.blit(text_litera,(900 + litera * 100, 950))
+
+        scor_text = font(35).render(f"Scor: {joc.scor}", True, "black")
+        scor_rect = scor_text.get_rect(center=(1600, 100))
+
+        scor_maxim_text = font(35).render(f"Scor maxim: {joc.scor_maxim}", True, "black")
+        scor_maxim_rect = scor_maxim_text.get_rect(center=(1600, 200))
+        ecran.blit(scor_maxim_text, scor_maxim_rect)
+
+        ecran.blit(scor_text, scor_rect)
+
+        continua_inapoi = Button(image=None, pos=(150, 1020), text_input="MENIU", font=font(50), base_color="black", hovering_color="white")
+        continua_indiciu = Button(image=None, pos=(100, 100), text_input="?", font=font(55), base_color="black", hovering_color="white")
+
+        continua_indiciu.changeColor(pozitie_mouse_continua)
+        continua_indiciu.update(ecran)
+
+        continua_inapoi.changeColor(pozitie_mouse_continua)
+        continua_inapoi.update(ecran)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if continua_inapoi.checkForInput(pozitie_mouse_continua):
+                    meniu_principal()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if continua_indiciu.checkForInput(pozitie_mouse_continua):
+                    indiciu(joc)
+        pygame.display.update()
+
+
+def indiciu(joc):
+
     while True:
         pozite_mouse_indiciu = pygame.mouse.get_pos()
 
-        ecran.blit(BG, (0, 0))
-
-        indiciu_text = font(45).render("Indiciul este...", True, "black")
-        indiciu_rect = indiciu_text.get_rect(center=(960, 300))
+        indiciu_text = font(35).render(f"Indiciul este: {joc.cuvant[joc.indexul_unei_litere]}", True, "black")
+        indiciu_rect = indiciu_text.get_rect(center=(300, 300))
         ecran.blit(indiciu_text, indiciu_rect)
 
-        indiciu_inapoi = Button(image=None, pos=(960, 500), text_input="INAPOI", font=font(86), base_color="blueviolet",
-                                hovering_color="blue")
+        indiciu_inapoi = Button(image=None, pos=(30, 300), text_input="x", font=font(35), base_color="black",
+                                hovering_color="white")
 
         indiciu_inapoi.changeColor(pozite_mouse_indiciu)
         indiciu_inapoi.update(ecran)
@@ -71,50 +119,50 @@ def indiciu():
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if indiciu_inapoi.checkForInput(pozite_mouse_indiciu):
-                    joaca()
+                    continua_1(joc)
         pygame.display.update()
-def optiuni():
-    while True:
-        pozite_mouse_optiuni = pygame.mouse.get_pos()
-
-        ecran.blit(BG, (0, 0))
-
-        optiuni_text = font(45).render("Aici vor fi instructiunile", True, "black")
-        optiuni_rect = optiuni_text.get_rect(center=(960, 300))
-        ecran.blit(optiuni_text, optiuni_rect)
-
-        optiuni_inapoi = Button(image=None, pos=(960, 500), text_input="INAPOI", font=font(86), base_color="blueviolet", hovering_color="blue")
-
-        optiuni_inapoi.changeColor(pozite_mouse_optiuni)
-        optiuni_inapoi.update(ecran)
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if optiuni_inapoi.checkForInput(pozite_mouse_optiuni):
-                    meniu_principal()
-        pygame.display.update()
+#def optiuni():
+#    while True:
+#        pozite_mouse_optiuni = pygame.mouse.get_pos()
+#
+#        ecran.blit(screenUpdate, (0, 0))
+#
+#        optiuni_text = font(45).render("indiciu - ", True, "black")
+#        optiuni_rect = optiuni_text.get_rect(center=(960, 300))
+#        ecran.blit(optiuni_text, optiuni_rect)
+#
+#        optiuni_inapoi = Button(image=None, pos=(960, 500), text_input="INAPOI", font=font(86), base_color="black", hovering_color="white")
+#
+#        optiuni_inapoi.changeColor(pozite_mouse_optiuni)
+#        optiuni_inapoi.update(ecran)
+#
+#        for event in pygame.event.get():
+#            if event.type == pygame.QUIT:
+#                pygame.quit()
+#                sys.exit()
+#            if event.type == pygame.MOUSEBUTTONDOWN:
+#                if optiuni_inapoi.checkForInput(pozite_mouse_optiuni):
+#                    meniu_principal()
+#        pygame.display.update()
 def meniu_principal():
 
     while True:
 
-        ecran.blit(BG,(0, 0))
+        ecran.blit(screenUpdate, (0, 0))
 
         pozitie_mouse_meniu = pygame.mouse.get_pos()
-        meniu_text = font(100).render("MENIUL PRINCIPAL", True, "blueviolet")
+        meniu_text = font(100).render("SPANZURATOAREA", True, "black")
         meniu_rect = meniu_text.get_rect(center=(960,100))
 
-        buton_start = Button(image=None, pos=(960, 250), text_input="START", font=font(75), base_color="blueviolet", hovering_color="blue")
+        buton_start = Button(image=None, pos=(700, 600), text_input="START", font=font(100), base_color="black", hovering_color="white")
 
-        buton_optiuni = Button(image=None, pos=(960, 350), text_input="INSTRUCTIUNI", font=font(76), base_color="blueviolet", hovering_color="blue")
+        #buton_optiuni = Button(image=None, pos=(960, 650), text_input="INSTRUCTIUNI", font=font(76), base_color="black", hovering_color="white")
 
-        buton_iesire = Button(image=None, pos=(960, 450), text_input="IESI", font=font(75), base_color="blueviolet", hovering_color="blue")
+        buton_iesire = Button(image=None, pos=(1200, 600), text_input="IESI", font=font(100), base_color="black", hovering_color="white")
 
         ecran.blit(meniu_text, meniu_rect)
 
-        for button in [buton_start, buton_optiuni, buton_iesire]:
+        for button in [buton_start, buton_iesire]:
             button.changeColor(pozitie_mouse_meniu)
             button.update(ecran)
 
@@ -125,12 +173,11 @@ def meniu_principal():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if buton_start.checkForInput(pozitie_mouse_meniu):
                     joaca()
-                if buton_optiuni.checkForInput(pozitie_mouse_meniu):
-                    optiuni()
+                #if buton_optiuni.checkForInput(pozitie_mouse_meniu):
+                #    optiuni()
                 if buton_iesire.checkForInput(pozitie_mouse_meniu):
                     pygame.quit()
                     sys.exit()
-
         pygame.display.update()
 
 meniu_principal()
--- main.py
+++ main.py
@@ -1,222 +1,146 @@
-import os  # Pentru A Sterge Consola
-import os.path
+import os
 import random
-import winsound # Pentru Audio
+import winsound
 from Cuvinte import set_cuvinte
+from ASCII__art import stages, logo
+import pygame
 
-# \n spune programului sa lase spatiu (endl in c++ sau <br> in html)
+class HangmanGame:
+    def __init__(self):
+        self.scor = 0
+        self.scor_maxim = self.incarca_scor_maxim()
+        self.lista = ""
+        self.cuvant = ""
+        self.numar_indicii = 2
+        self.vieti = 6
+        self.linii = []
 
-def puneDesenul():
-  # Desenele ce reprezinta "spanzuratoarea" pe fiecare stadiu
-  from ASCII__art import stages
-  print(stages[vieti])  # De fixat, sare peste anumite stagii finale
+    def ghiceste_litera(self, litera_introdusa):
+      for pozitie, litera in enumerate(self.cuvant):
+        if litera == litera_introdusa:
+          self.linii[pozitie] = litera
+    def incarca_scor_maxim(self):
+        if not os.path.exists('Salvari/'):
+            os.makedirs('Salvari/')
 
-# Arata o litera din cuvant
-def indiciu():
-  indiciu = ""
-  numar_litere = len(cuvant)
-  indexul_unei_litere = random.randint(0, numar_litere - 1)
-
-  print(f"Indiciul este: {cuvant[indexul_unei_litere]}")
-
-#Alege o cheie random si o valoare random din dictionar
-
-def alegeCuvantRandom():
-  global lista
-  global cuvant
-  lista = random.choice(list(set_cuvinte.keys()))
-  cuvant = random.choice(set_cuvinte[lista])
-
-# Initializeaza Scorul
-scor = 0
-
-# Create a new folder
-if not os.path.exists('Salvari/'):
-  os.makedirs('Salvari/')
-
-  # Create a new file inside the folder
-  with open('Salvari/Scor.txt', 'w') as f:
-    f.write(f"{scor}")
-    scorMaxim = 0
-    f.close()
-
-else:
-  # Create a new file inside the folder
-  f = open("Salvari/Scor.txt", "r")
-  scorSalvat = f.read()
-  scorMaxim = int(scorSalvat)
-  f.close()
-
-def tineScor():
-  global scor
-  global scorMaxim
-  if (numar_indicii < 2):
-    scor += 50
-  else:
-    scor += 100
-
-  # Create a new file inside the folder
-  print(f"Scorul tau este {scor}")
-
-  if (scorMaxim >= scor):
-    print(f"Scorul tau maxim este {scorMaxim}")
-  else:
-    print(f"Scorul tau maxim este {scor}")
-
-# Meniul Principal, Turorial
-def mainGameIntroduction():
-  from ASCII__art import logo
-  print(logo)
-  print("\n")
-  print("Bine ati venit la jocul de spanzuratoare!")
-  print("Daca te blochezi la un cuvant, scrie indiciu in terminal!")
-  print("Ai doua indicii pe runda! \n")
-  print("Daca ti-ai dat seama cuvantul, scrie-l! \n")
-  input("Apasa enter pentru a incepe jocul!").lower()
-  os.system('cls')
-  mainGame()
-
-
-# Jocul Principal
-def mainGame():
-  os.system('cls')
-  
-  # Numarul 1 este pentru a se auzi in background, de mentionat: functia nu suporta decat format .wav
-  winsound.PlaySound('Audio/Game Win Sound.wav', 1)
-
-  # Pregatirea jocului
-
-  alegeCuvantRandom()
-  
-  global numar_indicii
-  numar_indicii = 2
-  
-  numar_litere = len(cuvant)
-
-  sfarsit_joc = False
-  global vieti
-  vieti = 6
-  linii = []
-  puneDesenul()
-
-  for _ in range(numar_litere):
-    linii += "_"
-
-  print(f"Domeniul : {lista} \n")
-  print(' '.join(linii) + ' \n')
-
-  # Principala functie a jocului
-  while not sfarsit_joc:
-    
-    # Citim litera de la utilizator
-    litera_introdusa = input("Ghiciti o litera: ").lower()
-    os.system('cls')
-
-    # Inlocuieste fiecare linie cu litera corespunzatoare sau de fapt..
-
-    for pozitie in range(numar_litere):
-      litera = cuvant[pozitie]
-
-      if litera == litera_introdusa:
-        linii[pozitie] = litera
-
-    # Da un indiciu 
-    # elif este similar cu else if in C++
-    if ((litera_introdusa == "indiciu") and (numar_indicii > 0)):
-      puneDesenul()
-
-      winsound.PlaySound('Audio/Game Hint Sound.wav', 1)
-      numar_indicii = numar_indicii - 1
-      
-      indiciu()
-
-      if (numar_indicii == 0):
-        print("Nu mai ai indicii ramase! \n")
-      else:
-        print(f"Mai ai {numar_indicii} indiciu ramas! \n")
-
-      print(f"{''.join(linii)} \n")
-      
-    elif ((litera_introdusa == "indiciu") and (numar_indicii <= 0)):
-        puneDesenul()
-        print("Ai epuizat deja numarul de indicii! \n")
-        print(f"{''.join(linii)} \n")
-
-    elif (litera_introdusa == "skip"):
-        litera_introdusa = cuvant
-        puneDesenul()
-        tineScor()
-        print("Ai ghicit cuvantul! \n")
-        print(f"{cuvant} \n")
-        sfarsit_joc = True
-
-    elif (litera_introdusa == cuvant):
-        puneDesenul()
-        tineScor()
-        print("Ai ghicit cuvantul! \n")
-        print(f"{cuvant} \n")
-        sfarsit_joc = True
-    else:
-      # Caz 1: Litera introdusa nu se gaseste in cuvant
-
-      if litera_introdusa not in cuvant:
-        vieti -= 1
-
-        if vieti > 0:
-          puneDesenul()
-          winsound.PlaySound('Audio/Game Error Sound.wav', 1)
-
-        print(f"Litera {litera_introdusa} nu face parte din cuvant.")
-
-        # Nu mai sunt vieti, runda s-a terminat
-        if vieti == 0:
-          winsound.PlaySound('Audio/Game Lose Sound.wav', 1)
-          os.system('cls')
-          sfarsit_joc = True
-          from ASCII__art import stages
-          print(stages[0])
-          print(f"Scorul tau este {scor} \n")
-          print("Ai pierdut.")
-          print(f"Cuvantul era {cuvant}. \n")
-
-      # Caz 2:  Nu au mai ramas spatii necompletate. Runda s-a terminat
-
-      elif "_" not in linii:
-        winsound.PlaySound('Audio/Game Win Sound.wav', 1)
-        sfarsit_joc = True
-        os.system('cls')
-        tineScor()
-        print("Ai ghicit cuvantul! \n")
-      # Caz 3:  Litera deja in cuvant
-
-      elif litera_introdusa in cuvant:
-        puneDesenul()
-        print(f"Litera {litera_introdusa} este corecta (sau este deja ghicita).")
-
-        if not sfarsit_joc:
-          winsound.PlaySound('Audio/Generic Game Notification Sound.wav', 1)
-
-      # Unesc toate elementele listei "linii" pentru a le transforma intr-un string
-      print(f"{''.join(linii)} \n")
-
-  # Daca s-a terminat runda, intreba jucatorul daca mai vrea sa continuie
-
-  if sfarsit_joc:
-    vreaSaContinuie = input("Vrei sa continui? Daca scrii da, jocul va continua! \n").lower()
-
-    # Daca da, continua
-    if (vreaSaContinuie == "da"):
-      sfarsit_joc = False
-      mainGame()
-    else:
-      with open('Salvari/Scor.txt', 'w') as f:
-        if (scorMaxim < scor):
-          f.write(f"{scor}")
-          f.close()
+            with open('Salvari/Scor.txt', 'w') as f:
+                f.write(f"{self.scor}")
+                scor_maxim = 0
+                f.close()
         else:
-          f.write(f"{scorMaxim}")
-          f.close()
+            f = open("Salvari/Scor.txt", "r")
+            scor_salvat = f.read()
+            scor_maxim = int(scor_salvat)
+            f.close()
 
+        return scor_maxim
+
+    def tine_scor(self):
+        if self.numar_indicii == 2:
+            self.scor += 250
+        elif self.numar_indicii == 1:
+            self.scor += 100
+        else:
+            self.scor += 50
+
+        print(f"Scorul tau este {self.scor}")
+
+        if self.scor_maxim >= self.scor:
+            print(f"Scorul tau maxim este {self.scor_maxim}")
+        else:
+            print(f"Scorul tau maxim este {self.scor}")
+
+    def pune_desenul(self):
+        print(stages[self.vieti])
+
+    def alege_cuvant_random(self):
+        self.lista = random.choice(list(set_cuvinte.keys()))
+        self.cuvant = random.choice(set_cuvinte[self.lista])
+
+    def indiciu(self):
+        self.indexul_unei_litere = random.randint(0, len(self.cuvant) - 1)
+        print(f"Indiciul este: {self.cuvant[self.indexul_unei_litere]}")
+
+    def main_game(self):
+        self.pune_desenul()
+        self.alege_cuvant_random()
+        self.linii = ["_" for _ in range(len(self.cuvant))]
+        print(f"Domeniul : {self.lista} \n")
+        print(' '.join(self.linii) + ' \n')
+
+        while True:
+            litera_introdusa = input("Ghiciti o litera: ").lower()
+            os.system('cls')
+
+            if litera_introdusa == "indiciu" and self.numar_indicii > 0:
+                self.pune_desenul()
+                winsound.PlaySound('Audio/Game Hint Sound.wav', 1)
+                self.numar_indicii -= 1
+                self.indiciu()
+                print(f"Mai ai {self.numar_indicii} indiciu ramas! \n")
+                print(f"{''.join(self.linii)} \n")
+
+            elif litera_introdusa == "indiciu" and self.numar_indicii <= 0:
+                self.pune_desenul()
+                print("Ai epuizat deja numarul de indicii! \n")
+                print(f"{''.join(self.linii)} \n")
+
+            elif litera_introdusa == "skip":
+                litera_introdusa = self.cuvant
+                self.pune_desenul()
+                self.tine_scor()
+                print(f"Ai ghicit cuvantul! \n{self.cuvant} \n")
+                break
+
+            elif litera_introdusa == self.cuvant:
+                self.pune_desenul()
+                self.tine_scor()
+                print(f"Ai ghicit cuvantul! \n{self.cuvant} \n")
+                break
+
+            else:
+
+                self.ghiceste_litera(litera_introdusa)
+                if litera_introdusa not in self.cuvant:
+                    self.vieti -= 1
+                    if self.vieti > 0:
+                        self.pune_desenul()
+                        winsound.PlaySound('Audio/Game Error Sound.wav', 1)
+                    print(f"Litera {litera_introdusa} nu face parte din cuvant.")
+
+                    if self.vieti == 0:
+                        winsound.PlaySound('Audio/Game Lose Sound.wav', 1)
+                        os.system('cls')
+                        self.pune_desenul()
+                        print(f"Scorul tau este {self.scor} \n")
+                        print("Ai pierdut.")
+                        print(f"Cuvantul era {self.cuvant}. \n")
+                        break
+
+                elif "_" not in self.linii:
+                    winsound.PlaySound('Audio/Game Win Sound.wav', 1)
+                    os.system('cls')
+                    self.tine_scor()
+                    print(f"Ai ghicit cuvantul! \n")
+                    break
+
+                elif litera_introdusa in self.cuvant:
+                    self.pune_desenul()
+                    print(f"Litera {litera_introdusa} este corecta (sau este deja ghicita).")
+                    winsound.PlaySound('Audio/Generic Game Notification Sound.wav', 1)
+
+                print(f"{''.join(self.linii)} \n")
+
+        vrea_sa_continue = input("Vrei sa continui? Daca scrii da, jocul va continua! \n").lower()
+
+        if vrea_sa_continue != "da":
+            with open('Salvari/Scor.txt', 'w') as f:
+                if self.scor_maxim < self.scor:
+                    f.write(f"{self.scor}")
+                else:
+                    f.write(f"{self.scor_maxim}")
 
 # Ruleaza Programul
-mainGameIntroduction()
+if __name__ == "__main__":
+    joc = HangmanGame()
+    joc.main_game()
