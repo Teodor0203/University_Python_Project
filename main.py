21a536d333075f7e8fb02777ab2bbeaae0cf8541
Teodor0203 <toderitateodor@gmail.com>
2024-01-12 16:12:16 +0200

    Am modificat cursorul
    Dar are bug :))))))

--- main.py
+++ main.py
@@ -1,29 +1,33 @@
 import pygame, sys
 from pygame import mixer
-from buton import Button
+from buton import Buton
 from spanzuratoare import HangmanGame
-
+from cursor import Cursor
 
 mixer.init()
 mixer.music.load("Audio/birds-19624.mp3")
-mixer.music.set_volume(0.5)
+mixer.music.set_volume(0)
 mixer.music.play()
 
 pygame.init()
 resolutiaEcranului = pygame.display.Info()
 # pygame.display.set_mode((infoObject.current_w, infoObject.current_h))
 
-
 ecran = pygame.display.set_mode((resolutiaEcranului.current_w, resolutiaEcranului.current_h))
 # ecran = pygame.display.set_mode((1700, 1000))
 pygame.display.set_caption("Spanzuratoarea")
 
+cursor = Cursor(cale_imagine="interfata/cursor.png", ecran=ecran, latime=50, inaltime=50)
+
 BG = pygame.image.load("interfata/bg2.jpg")
 screenUpdate = pygame.transform.scale(BG, (resolutiaEcranului.current_w, resolutiaEcranului.current_h))
+
+BG_2 =  pygame.image.load("interfata/background3.png")
+screenUpdate_2 = pygame.transform.scale(BG_2, (resolutiaEcranului.current_w, resolutiaEcranului.current_h))
 def font(size):
     return pygame.font.Font("interfata/font2.otf", size)
 
-def joaca():
+def meniu_dificultate():
     joc = HangmanGame()
     joc.alege_cuvant_random()
     joc.indiciu()
@@ -31,28 +35,34 @@ def joaca():
     while True:
         pozitie_mouse_joca = pygame.mouse.get_pos()
 
-        ecran.blit(screenUpdate, (0, 0))
+        ecran.blit(screenUpdate_2, (0, 0))
 
         joaca_text = font(42).render("Bine ati venit la jocul de spanzuratoare!!", True, "black")
         joaca_rect = joaca_text.get_rect(center=(resolutiaEcranului.current_w / 2, 0 + (resolutiaEcranului.current_w * .1)))
 
-       #SA AFISEZE DOMENIUL PE ECRAN, DAR TREBUIE PUS LA INCEPUTUL FIECAREI RUNDE, NU IN MENIUL "JOACA"
+        dificultate_greu = Buton(imagine=None, pos=(resolutiaEcranului.current_w / 2, resolutiaEcranului.current_h / 2),
+                                 text_input="GREU", font=font(70), culoare_baza="black", culoare_activare=("white"))
+        dificultate_greu.schimbaCuloare(pozitie_mouse_joca)
+        dificultate_greu.update(ecran)
 
-       #domeniu_text = font(42).render(f"Domeniul este: {joc.lista}", True, "black")
-       #domeniu_rect = domeniu_text.get_rect(center=(resolutiaEcranului.current_w / 2, 0 +(resolutiaEcranului.current_w * 0.15)))
+        dificultate_USOR = Buton(imagine=None,
+                                 pos=(resolutiaEcranului.current_w / 2, resolutiaEcranului.current_h / 1.7),
+                                 text_input="USOR", font=font(70), culoare_baza="black", culoare_activare=("white"))
+        dificultate_USOR.schimbaCuloare(pozitie_mouse_joca)
+        dificultate_USOR.update(ecran)
 
-       #ecran.blit(domeniu_text, domeniu_rect)
+
         ecran.blit(joaca_text, joaca_rect)
 
-        joaca_inainte = Button(image=None, pos=(resolutiaEcranului.current_w / 3, resolutiaEcranului.current_h / 1.7), text_input="INAINTE", font=font(55), base_color="black", hovering_color="white")
-        joaca_inapoi = Button(image=None, pos=(resolutiaEcranului.current_w / 1.5, resolutiaEcranului.current_h / 1.7), text_input="INAPOI", font=font(55), base_color="black", hovering_color="white")
+        #joaca_inainte = Buton(image=None, pos=(resolutiaEcranului.current_w / 3, resolutiaEcranului.current_h / 1.7), text_input="INAINTE", font=font(55), base_color="black", hovering_color="white")
+        joaca_inapoi = Buton(imagine=None, pos=(resolutiaEcranului.current_w / 2, resolutiaEcranului.current_h / 1.2),
+                             text_input="INAPOI", font=font(70), culoare_baza="black", culoare_activare="white")
 
 
-        joaca_inainte.changeColor(pozitie_mouse_joca)
-        joaca_inainte.update(ecran)
+        #joaca_inainte.changeColor(pozitie_mouse_joca)
+        #joaca_inainte.update(ecran)
 
-
-        joaca_inapoi.changeColor(pozitie_mouse_joca)
+        joaca_inapoi.schimbaCuloare(pozitie_mouse_joca)
         joaca_inapoi.update(ecran)
 
         for event in pygame.event.get():
@@ -60,14 +70,17 @@ def joaca():
                 pygame.quit()
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if joaca_inapoi.checkForInput(pozitie_mouse_joca):
+                if joaca_inapoi.verificaInput(pozitie_mouse_joca):
                     meniu_principal()
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if joaca_inainte.checkForInput(pozitie_mouse_joca):
+                if dificultate_USOR.verificaInput(pozitie_mouse_joca):
                     joc.alegeNouCuvant()
-                    continua_1(joc)
+                    joc_principal(joc)
+
+        cursor.update()
         pygame.display.update()
-def continua_1(joc):
+
+def joc_principal(joc):
     while True:
 
         imp = pygame.image.load("interfata/stages/stage_0.png")
@@ -132,13 +145,15 @@ def continua_1(joc):
         ecran.blit(test_text, (resolutiaEcranului.current_w * .545, 300))
 
 
-        continua_inapoi = Button(image=None, pos=(150, resolutiaEcranului.current_h - 900), text_input="MENIU", font=font(50), base_color="black", hovering_color="white")
-        continua_indiciu = Button(image=None, pos=(100, 100), text_input="?", font=font(55), base_color="black", hovering_color="white")
+        continua_inapoi = Buton(imagine=None, pos=(150, resolutiaEcranului.current_h - 900), text_input="MENIU",
+                                font=font(50), culoare_baza="black", culoare_activare="white")
+        continua_indiciu = Buton(imagine=None, pos=(100, 100), text_input="?", font=font(55), culoare_baza="black",
+                                 culoare_activare="white")
 
-        continua_indiciu.changeColor(pozitie_mouse_continua)
+        continua_indiciu.schimbaCuloare(pozitie_mouse_continua)
         continua_indiciu.update(ecran)
 
-        continua_inapoi.changeColor(pozitie_mouse_continua)
+        continua_inapoi.schimbaCuloare(pozitie_mouse_continua)
         continua_inapoi.update(ecran)
 
 
@@ -151,10 +166,10 @@ def continua_1(joc):
                 pygame.quit()
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if continua_inapoi.checkForInput(pozitie_mouse_continua):
+                if continua_inapoi.verificaInput(pozitie_mouse_continua):
                     meniu_principal()
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if continua_indiciu.checkForInput(pozitie_mouse_continua):
+                if continua_indiciu.verificaInput(pozitie_mouse_continua):
                     indiciu(joc)
 
             if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE) or (event.type == pygame.KEYDOWN and event.key == pygame.K_PRINTSCREEN):
@@ -163,6 +178,8 @@ def continua_1(joc):
                 char = event.unicode if event.unicode else chr(event.key)
                 ghiceste_litera(char, joc)
 
+
+        cursor.update()
         ecran.blit(imp, (0, 550))
         pygame.display.flip()
         #pygame.display.update()
@@ -192,10 +209,10 @@ def indiciu(joc):
         indiciu_rect = indiciu_text.get_rect(center=(300, 300))
         ecran.blit(indiciu_text, indiciu_rect)
 
-        indiciu_inapoi = Button(image=None, pos=(30, 300), text_input="x", font=font(35), base_color="black",
-                                hovering_color="white")
+        indiciu_inapoi = Buton(imagine=None, pos=(30, 300), text_input="x", font=font(35), culoare_baza="black",
+                               culoare_activare="white")
 
-        indiciu_inapoi.changeColor(pozite_mouse_indiciu)
+        indiciu_inapoi.schimbaCuloare(pozite_mouse_indiciu)
         indiciu_inapoi.update(ecran)
 
         #ATUNCI CAND APASAM PE "?" CA SA APARA INDICIUL, IMAGINEA CU SPANZURATOAREA DISPAREA. ASA NU MAI DISPARE
@@ -230,8 +247,11 @@ def indiciu(joc):
                 pygame.quit()
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if indiciu_inapoi.checkForInput(pozite_mouse_indiciu):
-                    continua_1(joc)
+                if indiciu_inapoi.verificaInput(pozite_mouse_indiciu):
+                    joc_principal(joc)
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                        joc_principal(joc)
         pygame.display.update()
 
 #def optiuni():
@@ -244,7 +264,7 @@ def indiciu(joc):
 #        optiuni_rect = optiuni_text.get_rect(center=(960, 300))
 #        ecran.blit(optiuni_text, optiuni_rect)
 #
-#        optiuni_inapoi = Button(image=None, pos=(960, 500), text_input="INAPOI", font=font(86), base_color="black", hovering_color="white")
+#        optiuni_inapoi = Buton(image=None, pos=(960, 500), text_input="INAPOI", font=font(86), base_color="black", hovering_color="white")
 #
 #        optiuni_inapoi.changeColor(pozite_mouse_optiuni)
 #        optiuni_inapoi.update(ecran)
@@ -262,9 +282,14 @@ def meniu_principal():
     testI = 0
     moveDown = False
 
+
     while True:
 
+        pos = pygame.mouse.get_pos()
+
         ecran.blit(screenUpdate, (0, 0))
+
+
 
         if testI > 15:
             moveDown = True
@@ -282,16 +307,18 @@ def meniu_principal():
 
         meniu_rect = meniu_text.get_rect(center=(resolutiaEcranului.current_w / 2, testI + (resolutiaEcranului.current_w * .1)))
 
-        buton_start = Button(image=None, pos=(resolutiaEcranului.current_w / 3, resolutiaEcranului.current_h / 1.7), text_input="START", font=font(100), base_color="black", hovering_color="white")
+        buton_start = Buton(imagine=None, pos=(resolutiaEcranului.current_w / 3, resolutiaEcranului.current_h / 1.7),
+                            text_input="START", font=font(100), culoare_baza="black", culoare_activare="white")
 
-        #buton_optiuni = Button(image=None, pos=(960, 650), text_input="INSTRUCTIUNI", font=font(76), base_color="black", hovering_color="white")
+        #buton_optiuni = Buton(image=None, pos=(960, 650), text_input="INSTRUCTIUNI", font=font(76), base_color="black", hovering_color="white")
 
-        buton_iesire = Button(image=None, pos=(resolutiaEcranului.current_w / 1.5, resolutiaEcranului.current_h / 1.7), text_input="IESI", font=font(100), base_color="black", hovering_color="white")
+        buton_iesire = Buton(imagine=None, pos=(resolutiaEcranului.current_w / 1.5, resolutiaEcranului.current_h / 1.7),
+                             text_input="IESI", font=font(100), culoare_baza="black", culoare_activare="white")
 
         ecran.blit(meniu_text, meniu_rect)
 
         for button in [buton_start, buton_iesire]:
-            button.changeColor(pozitie_mouse_meniu)
+            button.schimbaCuloare(pozitie_mouse_meniu)
             button.update(ecran)
 
         for event in pygame.event.get():
@@ -299,13 +326,15 @@ def meniu_principal():
                 pygame.quit()
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if buton_start.checkForInput(pozitie_mouse_meniu):
-                    joaca()
+                if buton_start.verificaInput(pozitie_mouse_meniu):
+                    meniu_dificultate()
                 #if buton_optiuni.checkForInput(pozitie_mouse_meniu):
                 #    optiuni()
-                if buton_iesire.checkForInput(pozitie_mouse_meniu):
+                if buton_iesire.verificaInput(pozitie_mouse_meniu):
                     pygame.quit()
                     sys.exit()
+
+        cursor.update()
         pygame.display.update()
 
 #CAND PIERZI/CASTIGI APARE O IMAGINE CU O PANCARDA PE CARE SCRIE "AI PIERDUT/CASTIGAT!" SI BUTON PENTRU A CONITNUA SAU A IESI
@@ -327,17 +356,17 @@ def pierdut(joc):
         joaca_rect.center = (910, 480)
         ecran.blit(joaca_text, joaca_rect)
 
-        pierdut_inainte = Button(image=None, pos=(825, 600), text_input="CONTINUA",
-                                 font=font(30), base_color="black", hovering_color="white")
-        pierdut_inapoi = Button(image=None, pos=(1050, 600), text_input="MENIU", font=font(30), base_color="black",
-                                hovering_color="white")
+        pierdut_inainte = Buton(imagine=None, pos=(825, 600), text_input="CONTINUA", font=font(30),
+                                culoare_baza="black", culoare_activare="white")
+        pierdut_inapoi = Buton(imagine=None, pos=(1050, 600), text_input="MENIU", font=font(30), culoare_baza="black",
+                               culoare_activare="white")
 
         pozitie_mouse = pygame.mouse.get_pos()
 
-        pierdut_inapoi.changeColor(pozitie_mouse)
+        pierdut_inapoi.schimbaCuloare(pozitie_mouse)
         pierdut_inapoi.update(ecran)
 
-        pierdut_inainte.changeColor(pozitie_mouse)
+        pierdut_inainte.schimbaCuloare(pozitie_mouse)
         pierdut_inainte.update(ecran)
 
         for event in pygame.event.get():
@@ -345,13 +374,15 @@ def pierdut(joc):
                 pygame.quit()
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if pierdut_inapoi.checkForInput(pozitie_mouse):
+                if pierdut_inapoi.verificaInput(pozitie_mouse):
                     meniu_principal()
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if pierdut_inainte.checkForInput(pozitie_mouse):
+                if pierdut_inainte.verificaInput(pozitie_mouse):
                     joc.reset_game_state()
                     joc.alegeNouCuvant()
-                    continua_1(joc)
+                    joc_principal(joc)
+
+        cursor.update()
         pygame.display.update()
 
 def castigat(joc):
@@ -370,17 +401,17 @@ def castigat(joc):
         joaca_rect.center = (910, 480)
         ecran.blit(joaca_text, joaca_rect)
 
-        castigat_inainte = Button(image=None, pos=(825, 600), text_input="CONTINUA",
-                                 font=font(30), base_color="black", hovering_color="white")
-        castigat_inapoi = Button(image=None, pos=(1050, 600), text_input="MENIU", font=font(30), base_color="black",
-                                hovering_color="white")
+        castigat_inainte = Buton(imagine=None, pos=(825, 600), text_input="CONTINUA", font=font(30),
+                                 culoare_baza="black", culoare_activare="white")
+        castigat_inapoi = Buton(imagine=None, pos=(1050, 600), text_input="MENIU", font=font(30), culoare_baza="black",
+                                culoare_activare="white")
 
         pozitie_mouse = pygame.mouse.get_pos()
 
-        castigat_inainte.changeColor(pozitie_mouse)
+        castigat_inainte.schimbaCuloare(pozitie_mouse)
         castigat_inainte.update(ecran)
 
-        castigat_inapoi.changeColor(pozitie_mouse)
+        castigat_inapoi.schimbaCuloare(pozitie_mouse)
         castigat_inapoi.update(ecran)
 
         for event in pygame.event.get():
@@ -388,13 +419,13 @@ def castigat(joc):
                 pygame.quit()
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if castigat_inapoi.checkForInput(pozitie_mouse):
+                if castigat_inapoi.verificaInput(pozitie_mouse):
                     meniu_principal()
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if castigat_inainte.checkForInput(pozitie_mouse):
+                if castigat_inainte.verificaInput(pozitie_mouse):
                     joc.reset_game_state()
                     joc.alegeNouCuvant()
-                    continua_1(joc)
+                    joc_principal(joc)
         pygame.display.update()
 
 meniu_principal()
