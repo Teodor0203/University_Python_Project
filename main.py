3a64465f05049afcee48a94aec831c028473b9de
Teodor0203 <toderitateodor@gmail.com>
2024-01-12 21:52:42 +0200

    Grafici nooi!!
    Am stat si am facut pixel art :D

--- main.py
+++ main.py
@@ -20,10 +20,10 @@ pygame.display.set_caption("Spanzuratoarea")
 
 cursor = Cursor(cale_imagine="interfata/cursor.png", ecran=ecran, latime=50, inaltime=50)
 
-BG = pygame.image.load("interfata/bg2.jpg")
+BG = pygame.image.load("interfata/horse0.png")
 screenUpdate = pygame.transform.scale(BG, (resolutia_ecranului.current_w, resolutia_ecranului.current_h))
 
-BG_2 = pygame.image.load("interfata/background3.png")
+BG_2 = pygame.image.load("interfata/horse10.png")
 screenUpdate_2 = pygame.transform.scale(BG_2, (resolutia_ecranului.current_w, resolutia_ecranului.current_h))
 
 def font(size):
@@ -37,23 +37,25 @@ def meniu_dificultate():
     while True:
         
         ecran.blit(screenUpdate,(0,0))
-        
-        
+
+        cautat = pygame.transform.scale(pygame.image.load("interfata/Mircea.png"), (350, 350))
+        cautat_rect = cautat.get_rect()
+        cautat_rect.center = (resolutia_ecranului.current_w * .1, resolutia_ecranului.current_h * .2)
+
+        ecran.blit(cautat, cautat_rect)
         pozitie_mouse_joca = pygame.mouse.get_pos()
 
-        
-
         joaca_text_1 = font(40).render("El este Mircea și urmează să fie judecat.", True, "black")
-        joaca_rect_1 = joaca_text_1.get_rect(center=(resolutia_ecranului.current_w * .5,  (resolutia_ecranului.current_h * .2)))
+        joaca_rect_1 = joaca_text_1.get_rect(center=(resolutia_ecranului.current_w * .6,  (resolutia_ecranului.current_h * .2)))
         
-        joaca_text_2 = font(40).render("Ghicește cuvantul pentru a-l salva.", True, "black")
+        joaca_text_2 = font(40).render("Ghicește cuvântul pentru a-l salva.", True, "black")
         joaca_rect_2 = joaca_text_2.get_rect(center=(resolutia_ecranului.current_w * .5,  (resolutia_ecranului.current_h * .3)))
 
         dificultate_GREU = Buton(imagine=None, pos=(resolutia_ecranului.current_w * 0.5, resolutia_ecranului.current_h * 0.6), text_input="GREU", font=font(75), culoare_baza="black", culoare_activare=("white"))
         dificultate_GREU.schimbaCuloare(pozitie_mouse_joca)
         dificultate_GREU.update(ecran)
 
-        dificultate_USOR = Buton(imagine=None, pos=(resolutia_ecranului.current_w * 0.5, resolutia_ecranului.current_h * 0.7), text_input="USOR", font=font(75), culoare_baza="black", culoare_activare=("white"))
+        dificultate_USOR = Buton(imagine=None, pos=(resolutia_ecranului.current_w * 0.5, resolutia_ecranului.current_h * 0.7), text_input="UȘOR", font=font(75), culoare_baza="black", culoare_activare=("white"))
         dificultate_USOR.schimbaCuloare(pozitie_mouse_joca)
         dificultate_USOR.update(ecran)
 
@@ -62,7 +64,7 @@ def meniu_dificultate():
         
 
         # joaca_inainte = Buton(imagine=None, pos=(resolutia_ecranului.current_w * .3, resolutia_ecranului.current_h * .6), text_input="INAINTE", font=font(75), culoare_baza="black", culoare_activare="white")
-        joaca_inapoi = Buton(imagine=None, pos=(resolutia_ecranului.current_w * .5, resolutia_ecranului.current_h * .9), text_input="INAPOI", font=font(75), culoare_baza="black", culoare_activare="white")
+        joaca_inapoi = Buton(imagine=None, pos=(resolutia_ecranului.current_w * .5, resolutia_ecranului.current_h * .9), text_input="ÎNAPOI", font=font(75), culoare_baza="black", culoare_activare="white")
 
         # joaca_inainte.schimbaCuloare(pozitie_mouse_joca)
         # joaca_inainte.update(ecran)
@@ -91,31 +93,31 @@ def meniu_dificultate():
         pygame.display.update()
 
 def arata_stadile(joc):
-    imp = pygame.image.load("interfata/stages/stage_0.png")
+    imp = pygame.image.load("interfata/stages/stage_00.png")
 
     if joc.aCastigat:
-            imp = pygame.image.load("interfata/stages/stage_8.png")
+            imp = pygame.image.load("interfata/stages/stage_08.png")
     else:
         if joc.vieti >= 6:
-            imp = pygame.image.load("interfata/stages/stage_0.png")
+            imp = pygame.image.load("interfata/stages/stage_00.png")
 
         if joc.vieti == 5:
-            imp = pygame.image.load("interfata/stages/stage_1.png")
+            imp = pygame.image.load("interfata/stages/stage_01.png")
 
         if joc.vieti == 4:
-            imp = pygame.image.load("interfata/stages/stage_2.png")
+            imp = pygame.image.load("interfata/stages/stage_02.png")
 
         if joc.vieti == 3:
-             imp = pygame.image.load("interfata/stages/stage_3.png")
+             imp = pygame.image.load("interfata/stages/stage_03.png")
 
         if joc.vieti == 2:
-            imp = pygame.image.load("interfata/stages/stage_4.png")
+            imp = pygame.image.load("interfata/stages/stage_04.png")
 
         if joc.vieti == 1:
-            imp = pygame.image.load("interfata/stages/stage_5.png")
+            imp = pygame.image.load("interfata/stages/stage_05.png")
 
         if joc.vieti <= 0:
-            imp = pygame.image.load("interfata/stages/stage_6.png")
+            imp = pygame.image.load("interfata/stages/stage_06.png")
 
     ecran.blit(imp, (0, resolutia_ecranului.current_h - 512))  
 
@@ -144,7 +146,7 @@ def joc_principal(joc):
     joc.runda_finalizata = False
 
     while True:
-        ecran.blit(screenUpdate, (0, 0))
+        ecran.blit(screenUpdate_2, (0, 0))
 
         if joc.vieti <= 0:
             joc.runda_finalizata = True
@@ -290,7 +292,7 @@ def meniu_principal():
             menu_offset += .05
 
         pozitie_mouse_meniu = pygame.mouse.get_pos()
-        meniu_text = font(100).render("SPANZURATOAREA", True, "black")
+        meniu_text = font(100).render("SPÂNZURĂTOAREA", True, "black")
         meniu_rect = meniu_text.get_rect(center=(resolutia_ecranului.current_w * .5, 0 + (resolutia_ecranului.current_w * .1)))
 
         meniu_rect = meniu_text.get_rect(center=(resolutia_ecranului.current_w * .5, menu_offset + (resolutia_ecranului.current_w * .1)))
@@ -299,7 +301,7 @@ def meniu_principal():
 
         #buton_optiuni = Buton(imagine=None, pos=(960, 650), text_input="INSTRUCTIUNI", font=font(76), culoare_baza="black", culoare_activare="white")
 
-        buton_iesire = Buton(imagine=None, pos=(resolutia_ecranului.current_w * .7, resolutia_ecranului.current_h * .6), text_input="IESI", font=font(100), culoare_baza="black", culoare_activare="white")
+        buton_iesire = Buton(imagine=None, pos=(resolutia_ecranului.current_w * .7, resolutia_ecranului.current_h * .6), text_input="IEȘI", font=font(100), culoare_baza="black", culoare_activare="white")
 
         ecran.blit(meniu_text, meniu_rect)
 
@@ -327,26 +329,26 @@ def meniu_principal():
 def pierdut(joc):
 
     while True:        
-        ecran.blit(screenUpdate, (0, 0))
+        ecran.blit(screenUpdate_2, (0, 0))
         main_game_gui(joc)
         arata_stadile(joc)
         #PANCARDA1 ESTE PENTRU AFISAJUL "AI PIERDUT/CASTIGAT!", IAR PANCARDA2 AR PUTEA FI FOLOSIT PENTRU TASTATURA
 
-        pancarda = pygame.image.load("interfata/pancarda1.png")
-        pancarda_2 = pygame.transform.scale(pancarda, (500, 250))
+        pancarda = pygame.image.load("interfata/semn_3.png")
+        pancarda_2 = pygame.transform.scale(pancarda, (600, 600))
         rect_pancarda = pancarda_2.get_rect()
         rect_pancarda.center = (resolutia_ecranului.current_w * .5, resolutia_ecranului.current_h * .5 + 80)
 
         ecran.blit(pancarda_2, rect_pancarda)
 
-        joaca_text = font(40).render("Ai pierdut!", True, "black")
+        joaca_text = font(40).render("Nu l-ai salvat!", True, "black")
         joaca_rect = joaca_text.get_rect()
         joaca_rect.center = (resolutia_ecranului.current_w * .5, resolutia_ecranului.current_h * .5)
         ecran.blit(joaca_text, joaca_rect)
 
-        pierdut_inainte = Buton(imagine=None, pos=(resolutia_ecranului.current_w * .5 - 80, resolutia_ecranului.current_h * .5 + 150), text_input="CONTINUA",
+        pierdut_inainte = Buton(imagine=None, pos=(resolutia_ecranului.current_w * .5 - 80, resolutia_ecranului.current_h * .5 + 130), text_input="CONTINUĂ",
                                  font=font(30), culoare_baza="black", culoare_activare="white")
-        pierdut_inapoi = Buton(imagine=None, pos=(resolutia_ecranului.current_w * .5 + 110, resolutia_ecranului.current_h * .5 + 150), text_input="MENIU", font=font(30), culoare_baza="black",
+        pierdut_inapoi = Buton(imagine=None, pos=(resolutia_ecranului.current_w * .5 + 110, resolutia_ecranului.current_h * .5 + 130), text_input="MENIU", font=font(30), culoare_baza="black",
                                 culoare_activare="white")
 
         pozitie_mouse = pygame.mouse.get_pos()
@@ -376,26 +378,26 @@ def pierdut(joc):
 
 def castigat(joc):
     while True:
-        ecran.blit(screenUpdate, (0, 0))
+        ecran.blit(screenUpdate_2, (0, 0))
         joc.aCastigat = True
         main_game_gui(joc)
         arata_stadile(joc)
 
-        pancarda = pygame.image.load("interfata/pancarda1.png")
-        pancarda_2 = pygame.transform.scale(pancarda, (500, 250))
-        rect_pancarda = pancarda_2.get_rect()
+        pancarda = pygame.image.load("interfata/semn_3.png")
+        #pancarda_2 = pygame.transform.scale(pancarda, (500, 250))
+        rect_pancarda = pancarda.get_rect()
         rect_pancarda.center = (resolutia_ecranului.current_w * .5, resolutia_ecranului.current_h * .5 + 80)
 
-        ecran.blit(pancarda_2, rect_pancarda)
+        ecran.blit(pancarda, rect_pancarda)
 
-        joaca_text = font(40).render("Ai ghicit!", True, "black")
+        joaca_text = font(40).render("L-ai salvatt!", True, "black")
         joaca_rect = joaca_text.get_rect()
         joaca_rect.center = (resolutia_ecranului.current_w * .5, resolutia_ecranului.current_h * .5)
         ecran.blit(joaca_text, joaca_rect)
 
-        castigat_inainte = Buton(imagine=None, pos=(resolutia_ecranului.current_w * .5 - 80, resolutia_ecranului.current_h * .5 + 150), text_input="CONTINUA",
+        castigat_inainte = Buton(imagine=None, pos=(resolutia_ecranului.current_w * .5 - 80, resolutia_ecranului.current_h * .5 + 130), text_input="CONTINUĂ",
                                  font=font(30), culoare_baza="black", culoare_activare="white")
-        castigat_inapoi = Buton(imagine=None, pos=(resolutia_ecranului.current_w * .5 + 110, resolutia_ecranului.current_h * .5 + 150), text_input="MENIU", font=font(30), culoare_baza="black",
+        castigat_inapoi = Buton(imagine=None, pos=(resolutia_ecranului.current_w * .5 + 110, resolutia_ecranului.current_h * .5 + 130), text_input="MENIU", font=font(30), culoare_baza="black",
                                 culoare_activare="white")
 
         pozitie_mouse = pygame.mouse.get_pos()
