dd2083130a13d7eff25e285e694940979934b29b
Teodor0203 <toderitateodor@gmail.com>
2024-01-13 15:46:32 +0200

    Update cuvinte.py

--- cuvinte.py
+++ cuvinte.py
@@ -2,16 +2,16 @@ cuvinte_usoare = {
 
 'animale' :['arici','aligator','acvila','barza','balena','bibilica','bizon','bursuc','camila','crocodil','cal','delfin','dihor','elefant','echidna','fazan','ghepard',
             'iepure','jder','jaguar','lebada','leopard','maimuta','nurca','castor','veverita','zebra','impala','tigru','leu','antilopa','caprioara','pisica','pantera','cangur','vulpe'],
-'masini' : ['volvo', 'mercedes', 'renault', 'subaru', 'volkswagen', 'hyundai', 'maserati', 'ferrari', 'porsche', 'tesla', 'bugatti', 'dodge', 'mustang'],
+'mașini' : ['volvo', 'mercedes', 'renault', 'subaru', 'volkswagen', 'hyundai', 'maserati', 'ferrari', 'porsche', 'tesla', 'bugatti', 'dodge', 'mustang'],
 
-'tari' : ['spania', 'romania', 'germania', 'finlanda', 'brazilia', 'cehoslovacia', 'belarus', 'norvegia', 'thailanda', 'malaesya', 'uzbekistan', 'qatar', 'vatican']
+'țări' : ['spania', 'romania', 'germania', 'finlanda', 'brazilia', 'cehoslovacia', 'belarus', 'norvegia', 'thailanda', 'malaesya', 'uzbekistan', 'qatar', 'vatican']
              
 }
 
 cuvinte_grele = { 
 
 'biologie' :['sternocleidomastoidian', 'mitocondrie', 'cloroplast', 'cromozom', 'hemoglobina', 'carboxihemoglobina', 'trombocit', 'stomata', 'falange', 'metacarpiene' , 'cubitus', 'humerus', 'clavicula', 'stern', 'femur', 'nicovala', 'laringe', 'bronhiola', 'alveola', 'clorofila', 'verterbra', 'epiderm', 'pleura', 'meninge', 'cefalorahidian', 'brahioradial', 'gastrocnemian', 'solear', 'leucocit'],
-'fizica' : ['entropie', 'ampermetru', 'volmetru', 'coulomb', 'fisiune', 'hertz', 'radioactivitate', 'refractie', 'tranzistor', 'kirchhoff', 'fahrenheit', 'relativitate', 'cuantic', 'proton', 'electron', 'watt', 'viscozitate', 'rugozitate', 'energie', 'electricitate', 'frecventa', 'interfranja', 'interferenta', 'fuziune', 'schrodinger', 'qubit', 'newton', 'presiune', 'termodinamica'],
+'fizică' : ['entropie', 'ampermetru', 'volmetru', 'coulomb', 'fisiune', 'hertz', 'radioactivitate', 'refractie', 'tranzistor', 'kirchhoff', 'fahrenheit', 'relativitate', 'cuantic', 'proton', 'electron', 'watt', 'viscozitate', 'rugozitate', 'energie', 'electricitate', 'frecventa', 'interfranja', 'interferenta', 'fuziune', 'schrodinger', 'qubit', 'newton', 'presiune', 'termodinamica'],
 
 'capitale' : ['sarajevo', 'reykjavik', 'jakarta', 'ottawa', 'canberra', 'tashkent', 'riyadh', 'bangkok', 'islamabad', '']
              
